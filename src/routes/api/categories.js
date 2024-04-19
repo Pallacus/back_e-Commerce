37a25dac20147b1fb9a325baf1f478dfecba5307
Sonia-Flores <sonia.fmayoral@gmail.com>
2024-04-19 09:20:18 +0200

    Add categoryById

--- src/routes/api/categories.js
+++ src/routes/api/categories.js
@@ -1,5 +1,12 @@
-const { getAllCategories, insertNewCategory, getCategoryById, updateCategory, deleteCategory } = require('../../models/categories.model');
-const { checkCategory } = require('../../helpers/category.middlewares');
+const {
+  getAllCategories,
+  insertNewCategory,
+  getCategoryById,
+  updateCategory,
+  deleteCategory,
+} = require("../../models/categories.model");
+const { checkCategory } = require("../../helpers/category.middlewares");
+const { checkToken } = require("../../helpers/users.middlewares");
 
 const router = require("express").Router();
 
@@ -8,6 +15,16 @@ router.get("/", async (req, res) => {
   try {
     const [categories] = await getAllCategories();
     res.json(categories);
+  } catch (error) {
+    res.json({ fatal: error.message });
+  }
+});
+
+// GET /categories
+router.get("/:categoryId", async (req, res) => {
+  try {
+    const [[category]] = await getCategoryById(req.params.categoryId);
+    res.json(category);
   } catch (error) {
     res.json({ fatal: error.message });
   }
@@ -27,7 +44,10 @@ router.post("/new", async (req, res) => {
 
 //PUT /categories/update/CATEGORYID
 router.put("/update/:categoryId", checkCategory, async (req, res) => {
-  const { params: { categoryId }, body: { title } } = req;
+  const {
+    params: { categoryId },
+    body: { title },
+  } = req;
   try {
     await updateCategory(categoryId, title);
     const [categories] = await getCategoryById(categoryId);
